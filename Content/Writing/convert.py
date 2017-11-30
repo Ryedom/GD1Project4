8cdd664915962df98adcee5db52eb3948fe0a626
DefectiveMan <pelaet@rpi.edu>
2017-11-30 13:46:16 -0500

    Adding Next Conversation, fixed a script A format problem, made it so convert.py doesnt convert itself.

--- Content/Writing/convert.py
+++ Content/Writing/convert.py
@@ -2,7 +2,7 @@ import os
 
 if __name__ == '__main__':
     for filename in os.listdir(os.getcwd()):
-        if filename == 'Ref.txt' or filename.find('Parsed') != -1:
+        if filename == 'Ref.txt' or filename.find('Parsed') != -1 or filename == 'convert.py':
             continue
         f = open(filename, 'r')
         writefile = open(filename[0:-4] + 'Parsed.txt', 'w')
