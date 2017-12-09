9874e2022eaa3bce76785850ac31a9ccc4d29290
DefectiveMan <pelaet@rpi.edu>
2017-12-09 13:26:04 -0500

    Dialog System Fully Functioning

--- Content/Writing/convert.py
+++ Content/Writing/convert.py
@@ -2,13 +2,20 @@ import os
 
 if __name__ == '__main__':
     for filename in os.listdir(os.getcwd()):
-        if filename == 'Ref.txt' or filename.find('Parsed') != -1 or filename == 'convert.py':
+        if filename == 'Ref.txt' or filename.find('Parsed') != -1 or filename == 'convert.py' or filename[0] == '.' or os.path.isdir(filename):
             continue
         f = open(filename, 'r')
         writefile = open(filename[0:-4] + 'Parsed.txt', 'w')
         filetext = f.read()
         filetext = filetext.strip()
-        no_newline = filetext.replace('\n', '<br>')
+        split_text = filetext.split('\n')
+        interim_list = []
+        for line in split_text:
+            if line[0] == 'n':
+                continue
+            interim_list.append(line)
+        result_string = "\n".join(interim_list)
+        no_newline = result_string.replace('\n', '<br>')
         writefile.write(no_newline)
         f.close()
         writefile.close()
